6ab2eae9f404cc110b6f7c8905d627d7ce445706
WooheeKim <121842658+WooheeKim@users.noreply.github.com>
2023-04-19 23:19:02 +0900

    Update boardform.js
    d

--- src/main/resources/static/js/boardform.js
+++ src/main/resources/static/js/boardform.js
@@ -5,15 +5,30 @@
  const submitButton=document.getElementById("submitButton");
  
  submitButton.addEventListener("click", function(){
-	 console('submit button click');
+// 	 console('submit button click');
 	 
-	 let chkTitle = document.getElementById('title').value;
-	 let chkWriter = document.getElementById('writer').value;
+// 	 let chkTitle = document.getElementById('title').value;
+// 	 let chkWriter = document.getElementById('writer').value;
 	
-	 if(chkTitle!='' && chkWriter!=''){
-		let frm = document.getElementById('contactForm');
-		frm.submit();
-	 }else{
-		alert('title, writer는 필수 입력값입니다.');
-	 }
+// 	 if(chkTitle!='' && chkWriter!=''){
+// 		let frm = document.getElementById('contactForm');
+// 		frm.submit();
+// 	 }else{
+// 		alert('title, writer는 필수 입력값입니다.');
+// 	 }
+	 
+	 //jQuery
+	let title = $('#title').val();
+	let writer = $('#writer').val();
+	let form = $('#contactForm');
+	 
+	 if(title==''){
+		alert('title check');
+		return false;
+	}else if(writer==''){
+		alert('writer check');
+		return false;
+	}else{
+		form.submit();
+	}
  });
